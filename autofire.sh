e72e9af7af0bc61a9109273078642c769535042d
dotslashed <73475291+dotslashed@users.noreply.github.com>
2021-06-10 15:48:08 +0530

    Create autofire.sh

---
+++ autofire.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+
+echo "-------------------------------------GOOGLE DORK AUTOMATION-------------------------------------------------------------------------------------"
+
+echo "+++++++++WARNING!!! This script will suddenly open many tabs on your browser+++++++++++"
+
+echo "[+] Please enter the target for whom you want to dork"
+
+read TARGET
+
+echo "[+] Dork started..."
+
+sleep 4
+
+echo "[+] Opening in browser..."
+
+sleep 5
+
+firefox https://www.google.com/search?q=site%3Ahttp%3A%2F%2Fcodepad.co+%22$TARGET%22
+
+firefox https://www.google.com/search?q=site%3Ahttp%3A%2F%2Fscribd.com+%22$TARGET%22
+
+firefox https://www.google.com/search?q=site%3Ahttp%3A%2F%2Fnpmjs.com+%22$TARGET%22
+
+firefox https://www.google.com/search?q=site%3Ahttp%3A%2F%2Fnpm.runkit.com+%22$TARGET%22
+
+firefox https://www.google.com/search?q=site%3Ahttp%3A%2F%2Flibraries.io+%22$TARGET%22
+
+firefox https://www.google.com/search?q=site%3Ahttp%3A%2F%2Fycombinator.com+%22$TARGET%22
+
+firefox https://www.google.com/search?q=site%3Ahttp%3A%2F%2Fcoggle.it+%22$TARGET%22
+
+firefox https://www.google.com/search?q=site%3Ahttp%3A%2F%2Fpapaly.com+%22$TARGET%22
+
+firefox https://www.google.com/search?q=site%3Ahttp%3A%2F%2Ftrello.com+%22$TARGET%22
+
+firefox https://www.google.com/search?q=site%3Ahttp%3A%2F%2Fprezi.com+%22$TARGET%22
+
+firefox https://www.google.com/search?q=site%3Ahttp%3A%2F%2Fjsdelivr.nrt+%22$TARGET%22
+
+firefox https://www.google.com/search?q=site%3Ahttp%3A%2F%2Fcodepen.io+%22$TARGET%22
+
+firefox https://www.google.com/search?q=site%3Ahttp%3A%2F%2Fcodeshare.io+%22$TARGET%22
+
+firefox https://www.google.com/search?q=site%3Ahttp%3A%2F%2Fsharecode.io+%22$TARGET%22
+
+firefox https://www.google.com/search?q=site%3Ahttp%3A%2F%2Fpastebin.com+%22$TARGET%22
+
+firefox https://www.google.com/search?q=site%3Ahttp%3A%2F%2Frepl.it+%22$TARGET%22
+
+firefox https://www.google.com/search?q=site%3Ahttp%3A%2F%2Fgitter.im+%22$TARGET%22
+
+firefox https://www.google.com/search?q=site%3Ahttp%3A%2F%2Fbitbucket.org+%22$TARGET%22
+
+firefox https://www.google.com/search?q=site%3Ahttp%3A%2F%2Fpastebin.com+%22$TARGET%22
+
+firefox https://www.google.com/search?q=inurl%3Agitlab+%22$TARGET%22
